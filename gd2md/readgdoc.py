e9f86dcfc6e1308205722a974092149fe9b9c8f5
Jeff Silberman <jsilberm@gmail.com>
2020-07-01 18:26:22 -0700

    typo

--- gd2md/readgdoc.py
+++ gd2md/readgdoc.py
@@ -1876,7 +1876,7 @@ if __name__ == '__main__':
     parser_a.add_argument('-f', '--full_docset', action='store_true', help='Generates the Main, MD and TXT folders')
     parser_a.add_argument('-i', '--id', required=True, nargs=1, help='Google Doc ID', metavar="")
     parser_a.add_argument('-m', '--meta_ignore', action='store_true', help='Ignores any metafile associated with this doc.')
-    parser_b.add_argument('-r', '--reference_path', nargs=1, help='Relative or Absolute path to images, used in md file, if obitted bitmap_destination is used', metavar="")
+    parser_a.add_argument('-r', '--reference_path', nargs=1, help='Relative or Absolute path to images, used in md file, if obitted bitmap_destination is used', metavar="")
     parser_a.add_argument('-v', '--verbose', action='store_true', help='Verbose text and with Screenshots')
     parser_a.add_argument('-w', '--write_cache', action='store_true', help='Stores the map liks to current dir, after successful run, needed by -c flag')
     add_syntax_inclusion(parser_name, "id,bitmap_destination, doc_destination")
